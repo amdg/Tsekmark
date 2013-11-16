40aef40aa92467817f4e25541535664adf6a9780
Anna Marseille Gabutero <amdg@lavabit.com>
2013-11-17 07:30:01 +0800

    Limit vote types to YES/NO only

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -13,18 +13,11 @@ class Vote < ActiveRecord::Base
 
   TYPE_UPVOTE = 1
   TYPE_DOWNVOTE = 2
-  TYPE_VAGUE = 3
-  TYPE_MEH = 0
 
   entity :id, :comment, :vote_type, :medium_photo, :thumb_photo do
     expose :user, :using => User::Entity
     expose :general_appropriation, :using => GeneralAppropriation::Entity
   end
-
-  #scope :upvote, where(vote_type: TYPE_UPVOTE)
-  #scope :downvote, where(vote_type: TYPE_DOWNVOTE)
-  #scope :vague, where(vote_type: TYPE_VAGUE)
-  #scope :meh, where(vote_type: TYPE_MEH)
 
   def medium_photo
     photo.url(:medium)
