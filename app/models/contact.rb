7edc4fab8b9f977cbddc134e4ef1633c123faebf
Anna Marseille Gabutero <amdg@lavabit.com>
2013-11-16 16:48:12 +0800

    Deleting more things we don't need

--- app/models/contact.rb
+++
@@ -1,5 +0,0 @@
-class Contact < ActiveRecord::Base
-  belongs_to :blaster, :polymorphic => true
-
-  attr_accessible :blaster, :provider, :provider_uid, :name, :email
-end
--- config/initializers/omnicontacts.rb
+++
@@ -1,7 +0,0 @@
-require "omnicontacts"
-
-Rails.application.middleware.use OmniContacts::Builder do
-  importer :gmail, Rails.application.config.google_app_id, Rails.application.config.google_app_secret, {:redirect_path => "/contacts/gmail/callback"}
-  importer :yahoo, Rails.application.config.yahoo_app_id, Rails.application.config.yahoo_app_secret, {:callback_path => '/contacts/yahoo/callback'}
-  importer :hotmail, Rails.application.config.hotmail_app_id, Rails.application.config.hotmail_app_secret, {:redirect_path => "/contacts/hotmail/callback"}
-end
