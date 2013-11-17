97355c6908d4f8c20a7e1259460557b1ce1d2ca3
Anna Marseille Gabutero <amdg@lavabit.com>
2013-11-17 11:40:47 +0800

    Delete more blaster stuff

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -1,19 +1,7 @@
 class Authentication < ActiveRecord::Base
-  attr_accessible :blaster, :token, :provider, :secret, :uid, :twitter_uid, :blaster_type, :blaster_id
-  belongs_to :blaster, :polymorphic => true
+  attr_accessible :token, :provider, :secret, :uid
 
   def self.create_with_oauth(oauth, user, context)
     authentication = Authentication.find_by_uid_and_provider(oauth.uid, oauth.provider)
-    unless authentication
-      blaster = context === 'biz' ? user.business : user
-      blaster.authentications.create!(
-        provider: oauth.provider,
-        token: oauth.credentials.token,
-        secret: oauth.credentials.secret,
-        uid: oauth.uid
-      )
-    end
   end
-
-
 end
