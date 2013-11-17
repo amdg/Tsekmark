0a7687086a2b6d55ebfb54150c4dafbfa11d8be1
Anna Marseille Gabutero <amdg@lavabit.com>
2013-11-17 10:25:49 +0800

    Add voting information for authenticated users

--- app/api/gas.rb
+++ app/api/gas.rb
@@ -21,7 +21,14 @@ module Gas
         response = search_client.search(options)
         result = response['hits']
         map_key = params[:fields] ? 'fields' : '_source'
-        { total: result['total'], from: params[:from] || 0, result: result['hits'].map { |hit| hit[map_key] } }
+        { total: result['total'], from: params[:from] || 0, result: result['hits'].map { |hit|
+          source = hit[map_key]
+          if current_user then
+            v = Vote.find_by_user_id_and_general_appropriation_id(current_user.id, source['budget_id'])
+            source['vote'] = v ? v.vote_type : nil
+          end
+          source
+        } }
       end
     end
   end
