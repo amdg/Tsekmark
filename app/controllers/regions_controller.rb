909089e0291748c3e3b2479c8f4c085c455b5f8a
Anna Marseille Gabutero <amdg@lavabit.com>
2013-11-17 10:26:11 +0800

    Use real numbers for region budget and project count

--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -1,20 +1,23 @@
 class RegionsController < ApplicationController
   layout 'clean'
+
   def index
-
-
   end
-
 
   def list
     areas = Area.all.inject([]) do |areas_arr, area|
+      size = GeneralAppropriation.where(area_id: area.id).sum(:budget_ps) +
+          GeneralAppropriation.where(area_id: area.id).sum(:budget_mooe) +
+          GeneralAppropriation.where(area_id: area.id).sum(:budget_co)
+      count = GeneralAppropriation.where(area_id: area.id).count
+
       stim_area = area.attributes
       stim_area.merge!({
                            upvotes: Forgery(:basic).number(:at_least => 250, :at_most => 800),
-                           size: Forgery(:basic).number(:at_least => 200000, :at_most => 3000000),
+                           size: size,
                            downvotes: Forgery(:basic).number(:at_least => 250, :at_most => 800),
                            buzz: Forgery(:basic).number(:at_least => 2, :at_most => 25),
-                           count: Forgery(:basic).number(:at_least => 5, :at_most => 25)})
+                           count: count})
 
       areas_arr << {name: area.name, children: [stim_area]}
     end
