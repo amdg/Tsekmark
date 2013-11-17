e4e25f8132470e0034165f5089ca4251890bd9e7
MJ Alvarez <mj.alvarez@belldigitalmedia.com.au>
2013-11-17 11:45:41 +0800

    Revert "Delete more blaster stuff"
    This reverts commit 97355c6908d4f8c20a7e1259460557b1ce1d2ca3.

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -1,7 +1,19 @@
 class Authentication < ActiveRecord::Base
-  attr_accessible :token, :provider, :secret, :uid
+  attr_accessible :blaster, :token, :provider, :secret, :uid, :twitter_uid, :blaster_type, :blaster_id
+  belongs_to :blaster, :polymorphic => true
 
   def self.create_with_oauth(oauth, user, context)
     authentication = Authentication.find_by_uid_and_provider(oauth.uid, oauth.provider)
+    unless authentication
+      blaster = context === 'biz' ? user.business : user
+      blaster.authentications.create!(
+        provider: oauth.provider,
+        token: oauth.credentials.token,
+        secret: oauth.credentials.secret,
+        uid: oauth.uid
+      )
+    end
   end
+
+
 end
